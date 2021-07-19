8a4af3f4d3147b8eac7e52b5e3efc06650ad5221
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2021-07-19 18:46:10 +0530

    Add project files.

---
+++ C-sharp-9-features/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_sharp_9_features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
